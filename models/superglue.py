2a416de4799afe90728e22a25f99a92e3dcff7fe
Pavel <kopanev-pavel-spb@yandex.ru>
2021-03-11 14:59:41 +0300

    Fixes to adapt for ROS

--- models/superglue.py
+++ models/superglue.py
@@ -62,7 +62,7 @@ def MLP(channels: list, do_bn=True):
 
 def normalize_keypoints(kpts, image_shape):
     """ Normalize keypoints locations based on image image_shape"""
-    _, _, height, width = image_shape
+    height, width = image_shape
     one = kpts.new_tensor(1)
     size = torch.stack([one*width, one*height])[None]
     center = size / 2
@@ -240,8 +240,8 @@ class SuperGlue(nn.Module):
             }
 
         # Keypoint normalization.
-        kpts0 = normalize_keypoints(kpts0, data['image0'].shape)
-        kpts1 = normalize_keypoints(kpts1, data['image1'].shape)
+        kpts0 = normalize_keypoints(kpts0, data['image_size0'])
+        kpts1 = normalize_keypoints(kpts1, data['image_size1'])
 
         # Keypoint MLP encoder.
         desc0 = desc0 + self.kenc(kpts0, data['scores0'])
